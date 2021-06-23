a5944b3ccd4cc17fd4201ff4b5bcfeccb9fd0152
Benjamin-Cervantez <71291911+Benjamin-Cervantez@users.noreply.github.com>
2021-06-23 12:07:56 -0700

    Add files via upload
    Manual version pomodoro

---
+++ main.py
@@ -0,0 +1,25 @@
+hours = float(input('How many hours do you have? '))
+minutes = float(input('How many minutes do you have? '))
+time = int(hours * 60 + minutes)
+temp = time
+shorts = int(0)
+longs = int(0)
+working = int(0)
+while temp > 0:
+    for x in range(4):
+        if temp > 0:
+            temp -= 25
+            working += 1
+        if temp > 0:
+            shorts += 1
+            temp -= 5
+    if temp > 0:
+            temp -= 25
+            working += 1
+    if temp > 0:
+        longs += 1
+        temp -= 30
+print("You have " + str(time) + " total minutes.")
+print("You have " + str(working) + " total work sessions.")
+print("You have " + str(shorts) + " short breaks.")
+print("You have " + str(longs) + " long breaks.")
